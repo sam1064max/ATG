a005379002c17f336cd94b7d6455df6a8d00f622
Sushant <sam1064max@gmail.com>
2016-03-28 20:21:22 +0000

    Making program compatible with python3

--- Reducer.py
+++ Reducer.py
@@ -19,24 +19,24 @@ def getSortedSuite(directory):
 
 def getMinimalSet(sortedSuite):
 	minSet = list()
-	keys = sortedSuite.keys()
+	keys = list(sortedSuite.keys())
 	key = keys[0]
 	minSet.append(key)
 	lineCoverage = [x["coverage"]["packages"]["package"]["classes"]["class"]["lines"]["line"] for x in sortedSuite.values()]
 	lines = len(lineCoverage[0])
 	cases = len(lineCoverage)
-	mat = [[int(lineCoverage[j][i]["@hits"]) for i in xrange(lines)] for j in xrange(cases)]
-	lineNumbers = [[int(lineCoverage[j][i]["@number"]) for i in xrange(lines)] for j in xrange(cases)]
+	mat = [[int(lineCoverage[j][i]["@hits"]) for i in range(lines)] for j in range(cases)]
+	lineNumbers = [[int(lineCoverage[j][i]["@number"]) for i in range(lines)] for j in range(cases)]
 	linesCovered = mat[0]
-	for i in xrange(1,cases):
-		newTest = [mat[i][j]+linesCovered[j] for j in xrange(lines)]
+	for i in range(1,cases):
+		newTest = [mat[i][j]+linesCovered[j] for j in range(lines)]
 		if linesCovered.count(0) > newTest.count(0):
 			linesCovered = newTest
 			minSet.append(keys[i])
 
-	for i in xrange(cases-1,-1,-1):
+	for i in range(cases-1,-1,-1):
 		if keys[i] in minSet:
-			newTest = [linesCovered[j]-mat[i][j] for j in xrange(lines)]
+			newTest = [linesCovered[j]-mat[i][j] for j in range(lines)]
 			if linesCovered.count(0) == newTest.count(0):
 				linesCovered = newTest
 				minSet.remove(keys[i])
@@ -46,10 +46,10 @@ def getMinimalSet(sortedSuite):
 #Main#
 sortedSuite = getSortedSuite(sys.argv[1])
 minSet = getMinimalSet(sortedSuite)
-print "Total number of lines : ",minSet[2]
-print "Number of lines not covered : ",len(minSet[1])
-print "lines which are not covered : ",minSet[1]
-print "Total number of Test-cases : ",minSet[3]
-print "Number of Test-cases selected : ",len(minSet[0])
-print "Test-cases selected : ",minSet[0]
+print("Total number of lines : ",minSet[2])
+print("Number of lines not covered : ",len(minSet[1]))
+print("lines which are not covered : ",minSet[1])
+print("Total number of Test-cases : ",minSet[3])
+print("Number of Test-cases selected : ",len(minSet[0]))
+print("Test-cases selected : ",minSet[0])
 #print(json.dumps(sortedSuite, indent=1))
--- Run.sh
+++ Run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 clang -emit-llvm -g -c $1 -o temp.bc
-klee --libc=uclibc --posix-runtime -output-dir=$3 temp.bc
+klee --libc=uclibc --posix-runtime -output-dir=$3 temp.bc > temp
+rm -fr temp temp.bc
 
+mkdir -p $4
 for f in "$3/*";
 do
 	grep -Eo --text 'program.*[a-zA-Z0-9]+' $f | grep -Eo --text '[a-zA-Z0-9]+$' >  "$4/$(basename $f)"
@@ -14,7 +16,7 @@ do
 	g++ -fprofile-arcs -ftest-coverage $2
 	./a.out < $f > temp
 	gcovr -r . --xml > "$5/$(basename $f).xml"
-	rm -fr a.out model.gcda model.gcno temp
+	rm -fr a.out *.gc* temp
 done
 
-python2.7 Parser.py "$5/*" > output
+python3 Reducer.py "$5/*" > output
