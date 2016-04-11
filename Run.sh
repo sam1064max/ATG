03f7b85ad15e96af49d8f689eb20d554fb671a1d
Sushant <sam1064max@gmail.com>
2016-04-11 12:08:54 +0000

    Adding options to Run.sh

--- Run.sh
+++ Run.sh
@@ -1,12 +1,56 @@
-#!/bin/bash
-clang -emit-llvm -g -c $1 -o temp.bc
-klee --libc=uclibc --posix-runtime temp.bc > temp
-rm -fr temp temp.bc
+#!/usr/bin/env bash 
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+TEST_GENERATOR="klee"
+PARSER="false"
+DELETE="false"
+
+while getopts 'o:p:d:' opt ; do
+  case $opt in
+    o) TEST_GENERATOR=$OPTARG ;;
+    p) PARSER=$OPTARG ;;
+	d) DELETE=$OPTARG ;;
+  esac
+done
+
+if [ $# -lt 2 ]; then
+  echo "Usage: $0 [options] klee_code c_code"
+  echo "   -o     Use other test-cases generator (def: $TEST_GENERATOR)"
+  echo "   -p     Use parser to make c code klee compatible (def: $PARSER)"
+  echo "   -d     Delete the temp files after completion (def: $DELETE)"
+  exit 1
+fi
+
+KLEE_CODE=$1
+C_CODE=$2
+
+if [ $TEST_GENERATOR != "klee" ]; then
+  echo "$TEST_GENERATOR is currently not available. Please use <klee>"
+  exit 1
+fi
+
+if [ $PARSER = "true" ]; then
+  echo "klee code converter is currently under process. Please convert the code manually."
+  exit 1
+fi
+
+mkdir -p log
+clang -emit-llvm -g -c $KLEE_CODE -o temp.bc
+klee --libc=uclibc --posix-runtime temp.bc > consol_output.txt
+mv -t log/ consol_output.txt temp.bc
 
 ./klee2test.sh "klee-out-0/*.ktest" "cases"
 
-./cases2xml.sh $2 "cases/*" "xml"
+./cases2xml.sh $C_CODE "cases/*" "xml"
 
 python3 Reducer.py "xml/*" > output
 
-#rm -fr output klee-last klee-out-* xml/ cases/
+cat output
+
+if [ $DELETE = "true" ]; then
+  rm -fr cases/ xml/ klee-*
+fi
+
--- cases2xml.sh
+++ cases2xml.sh
@@ -2,8 +2,8 @@ mkdir -p $3
 for f in $2;
 do
 	g++ -fprofile-arcs -ftest-coverage $1
-	./a.out < $f > temp
+	./a.out < $f > log/gcovr_consol_output.log
 	gcovr -r . --xml > "xml/$(basename $f).xml"
-	rm -fr a.out *.gc* temp
+	mv -t log/ a.out *.gc*
 done
 
