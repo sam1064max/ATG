8a9303635c011e29b6c84ec7a6596bca989631ae
Sushant <sam1064max@gmail.com>
2016-04-12 16:23:04 +0530

    Adding new examples

---
+++ kleecode/countdigits.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+  int n,count=0;
+  printf("Enter an integer: ");
+  klee_make_symbolic(&n, sizeof n, "n");
+  while(n!=0)
+  {
+      n/=10;             /* n=n/10 */
+      ++count;
+  }
+  printf("Number of digits: %d",count);
+}
